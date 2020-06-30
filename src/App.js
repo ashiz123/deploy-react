83f2590f319f908df548a263202daddf361d0a5f
DESKTOP-PRBKQSV\ashiz <ashizh@khozinfo.com>
2020-06-30 14:01:06 +0100

    commiting first time

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
+         This is ashiz app
         </a>
       </header>
     </div>
