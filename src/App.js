2f2c12faa11296faab2e8d6d067d9456e6ada12c
DESKTOP-PRBKQSV\ashiz <ashizh@khozinfo.com>
2020-06-30 15:20:41 +0100

    solving issue class name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-       <div class="container">
+       <div className="container">
           this is testing ashiz
        </div>
     </div>
